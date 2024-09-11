283a973f702b672251280637859fdfbc4d16259f
Imam Pratama Setiady <88639642+mamprat@users.noreply.github.com>
2024-09-11 20:33:44 +0700

    Update sensor_arus_reset_ethernet.ino

--- sensor_arus_reset_ethernet/sensor_arus_reset_ethernet.ino
+++ sensor_arus_reset_ethernet/sensor_arus_reset_ethernet.ino
@@ -12,25 +12,87 @@ const int threshold = 100;
 static unsigned long OnDelay;
 unsigned long TimeOn = 20000;  // 30 Seconds
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-EthernetServer server (80); // port HTTP
-IPAddress ip (192, 168, 1, 177); // IP untuk melihat di lokal
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+char server[] = "192, 168, 1, 177";  // Ganti dengan nama domain atau IP server
+//IPAddress ip(192, 168, 1, 177);
+EthernetClient client;
 
 void setup() {
   Serial.begin(9600);
   delay(100);
   emon1.current(sensor, 600.600);
 
-  Ethernet.begin(mac, ip);
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
+  //  Ethernet.begin(mac, ip);
+  Ethernet.begin(mac);
+  delay(1000);
+
+  // Cek koneksi
+  if (client.connect(server, 80)) {
+    Serial.print("server is at ");
+    //    Serial.print(Ethernet.localIP());
+  } else {
+    Serial.println("Connection failed");
+  }
   OnDelay = 0 - TimeOn;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  if (Irms > threshold) {
+    OnDelay = millis();
+  }
 
+  // Kirim data ke server
+  if (client.connected()) {
+    if (millis() - OnDelay >= (TimeOn)) {
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: ");
+      client.print(server);
+      client.print("\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(String(Irms).length());
+      client.print("\n\n");
+      client.print("value=");
+      client.print(Irms);
+      delay(500);
+    } else {
+      client.print("POST /update HTTP/1.1\n");
+      client.print("Host: ");
+      client.print(server);
+      client.print("\n");
+      client.print("Content-Type: application/x-www-form-urlencoded\n");
+      client.print("Content-Length: ");
+      client.print(String(Irms).length());
+      client.print("\n\n");
+      client.print("value=");
+      client.print(Irms);
+      delay(500);
+    }
+    delay(5000); // Tunggu 10 detik sebelum mengirim data lagi
+  }
 }
+
+//======================================================================
+//void loop() {
+//  int Irms = emon1.calcIrms(1480);  // Calculate Irms only
+//  if (Irms > threshold) {
+//    OnDelay = millis();
+//  }
+//  if (client.connected()) {
+//    client.print(server);
+//    client.print("\n");
+//    if (millis() - OnDelay >= (TimeOn)) {
+//      client.print(String(Irms).length());
+//      client.print("\n\n");
+//      client.println(Irms);
+//      delay(500);
+//    } else {
+//      client.print(String(Irms).length());
+//      client.print("\n\n");
+//      client.println(Irms);
+//      delay(500);
+//    }
+//  }
+//  delay(5000);
+//}
