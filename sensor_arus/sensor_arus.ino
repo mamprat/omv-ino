bebbbe2b1923089122a683e3c44c16fb5d019ad6
Imam Pratama Setiady <88639642+mamprat@users.noreply.github.com>
2024-09-11 19:13:15 +0700

    Add files via upload
    sensor arus ACS712

---
+++ sensor_arus/sensor_arus.ino
@@ -0,0 +1,34 @@
+//======= sensor arus ==========
+#include "EmonLib.h"
+
+// //======= sensor =======
+const int sensor = 1;  //A1
+
+//======= Inisialisasi =======
+EnergyMonitor emon1;
+const int threshold = 100;
+
+static unsigned long OnDelay;
+unsigned long TimeOn = 20000;  // 30 Seconds
+
+ void setup() {
+   Serial.begin(9600);
+   delay(100);
+   emon1.current(sensor, 600.600);  
+   OnDelay = 0 - TimeOn;
+ }
+
+void loop() {
+  int Irms = emon1.calcIrms(1480);  // Calculate Irms only
+  if (Irms > threshold) {
+    OnDelay = millis();
+  }
+
+  if (millis() - OnDelay >= (TimeOn)) {
+    Serial.println(Irms);
+    delay(500);
+  } else {
+    Serial.println(Irms);
+    delay(500);
+  }
+}
